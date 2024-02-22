9525f296ea5b7a77f8cffa945c85a103cd920434
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-02-22 16:25:43 +0300

    update route handler function to use async wait syntax

--- controllers/notes.js
+++ controllers/notes.js
@@ -6,12 +6,10 @@ const Note = require('../models/note')
 
 
 
-notesRouter.get('/', (request, response) => {
-  Note
-    .find({})
-    .then(notes => {
-      response.json(notes)
-    })
+notesRouter.get('/', async (request, response) => {
+
+  const notes = await Note.find({})
+  response.json(notes)
 })
 
 notesRouter.get('/:id', (request, response, next) => {
